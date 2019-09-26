720ed58a8aa5569e38462bd505e72880625e8237
795311 <795311@seq.org>
2019-09-26 10:28:11 -0700

    did changes

--- Projects/Lab924ScannerTest/Person.java
+++ Projects/Lab924ScannerTest/Person.java
@@ -8,14 +8,15 @@
 public class Person extends ScannerTest
 {
     // instance variables - replace the example below with your own
-    public String fName, Mname, lName;
+    public String fName, mName, lName;
+    public int space;
 
     /**
      * Constructor for objects of class Person
      */
     public Person(String fullName)
     {
-       parseName(fullName);
+        parseName(fullName);
     }
 
     /**
@@ -26,8 +27,26 @@ public class Person extends ScannerTest
      */
     public void  parseName(String fullName)
     {
-      fName = fullName.substring(0, fullName.indexOf(" "));
-      lName = fullName.substring(fullName.length()-1, fullName.indexOf(" "));
-      System.out.print(fName, lName);
+        int space = fullName.indexOf(" ");
+        int secondspace = fullName.lastIndexOf(" ");
+        int comma = fullName.indexOf(",");
+        if(!(secondspace == space)){
+            fName = fullName.substring(0, space);
+            lName = fullName.substring(secondspace+1, fullName.length());
+            mName = fullName.substring(space+1, secondspace);
+        }else{
+            fName = fullName.substring(0, space);
+            lName = fullName.substring(space+1, fullName.length());
+            mName = "";
+        }
+        if(!(comma == -1)){
+            lName = fullName.substring(0, comma);
+            fName = fullName.substring(comma+1, fullName.length());
+            mName = "";
+        }else{
+         lName = fullName.substring(0, comma);
+         fName = fullName.substring(comma+2, secondspace);
+         mName = fullName.substring(secondspace , fullName.length());
+        }
     }
 }
---
+++ Projects/NestedLoopsLab918/NestedLoops.java
@@ -0,0 +1,24 @@
+
+/**
+ * Write a description of class NestedLoops here.
+ *
+ * @author (Justin Mills)
+ * @version (a version number or a date)
+ */
+public class NestedLoops
+{
+    
+
+    /**
+     * Constructor for objects of class NestedLoops
+     */
+    public NestedLoops()
+    {
+        for(int outer = 1; outer <=5; outer++){
+            for(int inner = 5; inner<=6; inner++)
+            System.out.print(inner);
+        }
+    }
+
+  
+}
